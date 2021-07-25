42327595457d2ba11672b3dd36068c74ca790fc5
Promal B <promal.barua.cse@gmail.com>
2021-07-26 02:07:17 +0600

    Initial Commit

---
+++ PracticeList/PracticeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeListApp.swift
+//  PracticeList
+//
+//  Created by Promal on 26/7/21.
+//
+
+import SwiftUI
+
+@main
+struct PracticeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
